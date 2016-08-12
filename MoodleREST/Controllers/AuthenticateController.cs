9ee334136d73b45da1881c79e70b3ecbf7b269de
Projeto Moodle CNAT <projetomoodlecnat@gmail.com>
2016-08-12 14:47:34 -0300

    12/08/2016. Implementada a autenticação do usuário com verificação de senha hasheada e os tratamentos de exceção pertinentes à operação.

--- MoodleREST/Controllers/AuthenticateController.cs
+++ MoodleREST/Controllers/AuthenticateController.cs
@@ -8,6 +8,7 @@ using System.Web;
 using System.Web.Http;
 using CryptSharp;
 using System.Reflection;
+using System.Runtime.Serialization;
 
 namespace MoodleREST.Controllers
 {
@@ -23,53 +24,150 @@ namespace MoodleREST.Controllers
                 String[,] connectionStrings = (String[,])HttpContext.Current.Application["ConnectionStrings"];
                 switch (HttpContext.Current.Request["databaseType"])
                 {
-                    case "mysql":
+                     case "mysql":
+                        using (MySqlConnection connection = new MySqlConnection(connectionStrings[selectedIndex, 0]))
+                        {
+                            if (HttpContext.Current.Request["password"].Equals(""))
+                            {
+                                throw new InvalidFieldException("user_password_null");
+                            }
+                            if (HttpContext.Current.Request["user"].Equals(""))
+                            {
+                                throw new InvalidFieldException("user_non_existent");
+                            }
+                            connection.Open();
+                            MySqlCommand sqc = new MySqlCommand("select id, username, password from mdl_user where username=@username", connection);
+                            MySqlParameter msqp = new MySqlParameter("@username", MySqlDbType.VarChar);
+                            msqp.Value = HttpContext.Current.Request["username"].ToString();
+                            sqc.Parameters.Add(msqp);
+                            sqc.Prepare();
+                            MySqlDataReader msqr = sqc.ExecuteReader();
+                            if (msqr.Read())
+                            {
+                                String passwordFromDB = msqr.GetString(2);
+                                Crypter crypter = Crypter.GetCrypter(passwordFromDB);
+                                if (passwordFromDB.Equals(crypter.Crypt(HttpContext.Current.Request["password"], passwordFromDB.TrimEnd().TrimStart())))
+                                {
+                                    output = String.Format("user_authenticated;{0};{1}", msqr.GetInt32(0), msqr.GetString(1));
+                                }
+                                else
+                                {
+                                    output = "user_login_incorrect";
+                                }
+                            }
+                            else
+                            {
+                                throw new DataException("user_non_existent");
+                            }
+                            connection.Close();
+                        }
                         break;
                     case "postgre":
                         using (NpgsqlConnection connection = new NpgsqlConnection(connectionStrings[selectedIndex, 0]))
                         {
+                            if (HttpContext.Current.Request["password"].Equals(""))
+                            {
+                                throw new InvalidFieldException("user_password_null");
+                            }
+                            if (HttpContext.Current.Request["user"].Equals(""))
+                            {
+                                throw new InvalidFieldException("user_non_existent");
+                            }
                             connection.Open();
-                            NpgsqlCommand sqc = new NpgsqlCommand("select username, password from mdl_user where username=@username", connection);
+                            NpgsqlCommand sqc = new NpgsqlCommand("select id, username, password from mdl_user where username=@username", connection);
                             sqc.Parameters.AddWithValue("@username", NpgsqlTypes.NpgsqlDbType.Varchar, HttpContext.Current.Request["username"].ToString());
                             sqc.Prepare();
                             NpgsqlDataReader msqr = sqc.ExecuteReader();
                             if (msqr.Read())
                             {
-                                String passwordFromDB = msqr.GetString(1);
-                                String cryptedPass = passwordFromDB.Substring(passwordFromDB.LastIndexOf(".") + 1);
-                                String hash = passwordFromDB.Substring(0, passwordFromDB.LastIndexOf("."));
+                                String passwordFromDB = msqr.GetString(2);
                                 Crypter crypter = Crypter.GetCrypter(passwordFromDB);
-                                PropertyInfo pi = crypter.Properties.GetType().GetProperties()[3];
-                                pi.SetValue(crypter.Properties, false, null);
-                                crypter.Properties.Add(CrypterOption.Variant, BlowfishCrypterVariant.Corrected);
-                                if (cryptedPass.Equals(crypter.Crypt(HttpContext.Current.Request["username"], hash)))
+                                if (passwordFromDB.Equals(crypter.Crypt(HttpContext.Current.Request["password"], passwordFromDB.TrimEnd().TrimStart())))
                                 {
-                                    output = "user_authenticated";
+                                    output = String.Format("user_authenticated;{0};{1}", msqr.GetInt32(0), msqr.GetString(1));
                                 }
                                 else
                                 {
-                                    output = "user_login_failed";
+                                    output = "user_login_incorrect";
                                 }
                             }
                             else
                             {
-                                output = "non_existant_user";
+                                throw new DataException("user_non_existent");
                             }
                             connection.Close();
                         }
                         break;
                     case "mssql":
+                        using (SqlConnection connection = new SqlConnection(connectionStrings[selectedIndex, 0]))
+                        {
+                            if (HttpContext.Current.Request["password"].Equals(""))
+                            {
+                                throw new InvalidFieldException("user_password_null");
+                            }
+                            if (HttpContext.Current.Request["user"].Equals(""))
+                            {
+                                throw new InvalidFieldException("user_non_existent");
+                            }
+                            connection.Open();
+                            SqlCommand sqc = new SqlCommand("select id, username, password from mdl_user where username=@username", connection);
+                            sqc.Parameters.AddWithValue("@username", HttpContext.Current.Request["username"].ToString());
+                            sqc.Prepare();
+                            SqlDataReader msqr = sqc.ExecuteReader();
+                            if (msqr.Read())
+                            {
+                                String passwordFromDB = msqr.GetString(2);
+                                Crypter crypter = Crypter.GetCrypter(passwordFromDB);
+                                if (passwordFromDB.Equals(crypter.Crypt(HttpContext.Current.Request["password"], passwordFromDB.TrimEnd().TrimStart())))
+                                {
+                                    output = String.Format("user_authenticated;{0};{1}", msqr.GetInt32(0), msqr.GetString(1));
+                                }
+                                else
+                                {
+                                    output = "user_login_incorrect";
+                                }
+                            }
+                            else
+                            {
+                                throw new DataException("user_non_existent");
+                            }
+                            connection.Close();
+                        }
                         break;
                     default:
-                        throw new ArgumentException("Argumento de tipo de base inválido!");
+                        throw new InvalidFieldException("database_type_invalid");
                 }
             }
             catch (Exception ex)
             {
-                output = "error";
+                output = ex.Message;
                 Console.WriteLine(ex.Message);
             }
             return output;
+        }
+
+        //PropertyInfo pi = crypter.Properties.GetType().GetProperties()[3];
+        //pi.SetValue(crypter.Properties, false, null);
+        //crypter.Properties.Add(CrypterOption.Variant, BlowfishCrypterVariant.Corrected);
+
+        [Serializable]
+        private class InvalidFieldException : Exception
+        {
+            public InvalidFieldException()
+            {
+            }
+
+            public InvalidFieldException(string message) : base(message)
+            {
+            }
+
+            public InvalidFieldException(string message, Exception innerException) : base(message, innerException)
+            {
+            }
+
+            protected InvalidFieldException(SerializationInfo info, StreamingContext context) : base(info, context)
+            {
+            }
         }
     }
 }
